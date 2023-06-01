b03686f837adb50d6b1755cb90a62d04b683421d
wabibaah <wabisernproject@gmail.com>
2023-06-01 13:51:19 +0000

    done with the home page listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -30,7 +33,9 @@ export default async function Home() {
           gap-8
         "
         >
-          <div className="">My future listings</div>
+          {listings.map((listing: any) => {
+            return <ListingCard currentUser={currentUser} key={listing.id} data={listing} />;
+          })}
         </div>
       </Container>
     </ClientOnly>
