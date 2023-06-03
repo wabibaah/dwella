b62f9e9243085b9fcd3c83fc475e377c6a195ae2
wabibaah <wabisernproject@gmail.com>
2023-06-03 18:37:57 +0000

    done with deleting reservations

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  reservationId?: string;
+}
+
+export async function DELETE(request: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  // we want to ensure that the only people who can delete the reservation should be the people who created it and the owners of the property (listing)
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
+    },
+  });
+
+  return NextResponse.json(reservation);
+}
